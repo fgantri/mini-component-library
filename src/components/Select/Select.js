aa80faa22b8c5a5d8022ccadfd441f714c79c3ab
Fouad Gantri <fouad@gantri.dev>
2025-03-08 21:46:49 +0100

    add an alternative select implementation

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -7,48 +7,64 @@ import { getDisplayedValue } from './Select.helpers';
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
+  const iconSize = 24;
 
   return (
     <Wrapper>
-      <span>{displayedValue}</span>
-      <Icon 
-        id="chevron-down"
-        strokeWidth={2}
-      />
-      <select value={value} onChange={onChange}>
+      <NativeSelect value={value} onChange={onChange}>
         {children}
-      </select>
+      </NativeSelect>
+      <SelectPresentation>
+        <span>{displayedValue}</span>
+        <IconWrapper style={{"--size": iconSize + "px"}}>
+          <Icon id="chevron-down" strokeWidth={2} size={iconSize} />
+        </IconWrapper>
+      </SelectPresentation>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
   position: relative;
-  display: flex;
-  align-items: center;
-  gap: 24px;
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`;
+
+const SelectPresentation = styled.div`
   padding: 12px 16px;
-  background-color: ${COLORS.transparentGray15};
+  padding-right: 52px;
   color: ${COLORS.gray700};
-  border: none;
+  background-color: ${COLORS.transparentGray15};
   border-radius: 8px;
-  font-size: 1rem;
-  width: fit-content;
 
-  &:has(select:focus) {
-    outline: 2px solid ${COLORS.primary};
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
   }
 
-  &:hover {
-    color: black;
-  }
-
-  select {
-    position: absolute;
-    inset: 0;
-    opacity: 0;
-    cursor: pointer;
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
   }
 `;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin-block: auto;
+  right: 10px;
+  height: var(--size);
+  width: var(--size);
+  pointer-events: none;
+`;
+
 
 export default Select;
